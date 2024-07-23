b87bb8ff23c9df0a45cbf62f3f7563a5732da8c4
freakssha <lestreng.begi@gmail.com>
2024-07-23 03:40:30 +0300

    fix prod images

--- js/landing.js
+++ js/landing.js
@@ -1,4 +1,4 @@
 body = document.querySelector('body');
-body.style.backgroundImage = "url('/assets/images/bg/about1-bg.svg')";
+body.style.backgroundImage = "url('../assets/images/bg/about1-bg.svg')";
 body.style.backgroundSize = "1024px"
 body.style.backgroundPosition = "100% -90px";
