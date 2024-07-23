72901300818909b6d6ff72b731fde5091e3cdea8
freakssha <lestreng.begi@gmail.com>
2024-07-23 07:59:05 +0300

    + bg profile

---
+++ js/profile-bg-bottom.js
@@ -0,0 +1,4 @@
+body = document.querySelector('body');
+body.style.backgroundImage = "url('../assets/images/bg/profile-bottom.svg')";
+body.style.backgroundSize = "cover"
+body.style.backgroundPosition = "bottom";
